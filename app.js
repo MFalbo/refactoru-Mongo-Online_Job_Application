e4f202229c8b5a82198f776c7cd7f1d6166bb644
Matthew Falbo <matthew.falbo@gmail.com>
2014-05-15 09:05:09 -0600

    Completed parts 3&4.  Can create new applicants from form, store them in the databse, and retrieve them to be displayed on an applicants page.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
+mongoose.connect('mongodb://localhost/Omega-3-Studios');
+
+var Applicant = mongoose.model('Applicant',{
+	name: String,
+	bio: String,
+	skills: [String],
+	years: Number,
+	why: String
+});
 
 app.get('/', function(req, res) {
 	res.render('index');
@@ -13,13 +23,31 @@ app.get('/', function(req, res) {
 
 // displays a list of applicants
 app.get('/applicants', function(req, res){
-	res.render('applicants')
+	Applicant.find({}, function(err,applicants){
+		if(err){
+			console.log(err);
+			res.send(500, "Error retrieving applicants");
+			return;
+		}
+		res.render('applicants',{applicants: applicants})
+	})
 });
 
 // creates and applicant
 app.post('/applicant', function(req, res){
+	
 	console.log(req.body);
-	res.redirect('/success');
+	
+	var applier = new Applicant({name: req.body.name, bio: req.body.bio, skills: req.body.skills, years: req.body.years, why: req.body.why});
+	
+	applier.save(function(err, data){
+		if(err){
+			console.log(err);
+			res.send(500, "Error creating a new applicant");
+			return;
+		}
+		res.redirect('/success');
+	});
 });
 
 app.get('/success', function(req,res){
